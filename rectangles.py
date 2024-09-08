d489810512295bcfae672c7a83ef9ccff0e2f51b
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2024-09-08 09:46:58 +0200

    Update rectangles.py

--- rectangles.py
+++ rectangles.py
@@ -1,4 +1,19 @@
 import pgzrun
+import random
 WIDTH=1000
 HEIGHT= 800
+def draw():
+    maxh=800
+    maxw=200
+    red=random.randint(0,255)
+    blue=random.randint(0,255)
+    green=random.randint(0,255)
+    for i in range(40):
+        object=Rect((0,0),(maxw,maxh))
+        object.center=500,400
+        screen.draw.rect(object,(red,green,blue))
+        maxw+=10
+        maxh-=10
+def update():
+    pass
 pgzrun.go()
