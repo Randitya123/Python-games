2473d2f56935526649804f6c9ec793073b75b29b
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2024-09-22 09:48:38 +0200

    Update bees.py

--- Bees game/bees.py
+++ Bees game/bees.py
@@ -11,20 +11,34 @@ gameover=False
 def draw():
     screen.fill("skyblue")
     bees.draw() 
-    flower.draw()             
+    flower.draw()    
+    screen.draw.text("Score:"+str(score),(450,100),fontsize=35)   
+    if gameover:
+        screen.fill("red")
+        screen.draw.text("Time is up and your Score is "+str(score),center=(500,400),fontsize=45)   
+
 def update():
     pass
+    global score
     if keyboard.a:
-        bees.x=bees.x-2
+        bees.x=bees.x-4
     if keyboard.d:
-        bees.x=bees.x+2
+        bees.x=bees.x+4
     if keyboard.w:
-        bees.y=bees.y-2
+        bees.y=bees.y-4  
     if keyboard.s:
-        bees.y=bees.y+2
+        bees.y=bees.y+4
+    if bees.colliderect(flower):
+        ra()
+        score+=1
+def time():
+    global gameover
+    gameover=True
+    
 def ra():
     flower.x=random.randint(0,1000)
     flower.y=random.randint(0,800)
-clock.schedule_interval(ra,3.0)
+clock.schedule_interval(ra,5.0)
+clock.schedule(time,60)
 ra()
 pgzrun.go()
